14f6918782ffa632c357eac99f55c98ca5c49e1b
Rubens Rech <rubensch@icloud.com>
2019-09-26 18:23:44 -0300

    refactoring

--- test/gain_algorithm.py
+++ test/gain_algorithm.py
@@ -10,37 +10,50 @@ import numpy as np
 def info(x):
     return x * np.log2(1/x)
 
-def main():
-    D = pd.read_csv('example.csv', sep=";")
-    targetAttr = 'Joga'
+def infoGain(D, targetAttr, attr):
+    # Calculate dataset (D) current entropy
+    currEntropy = D.groupby(targetAttr).size().apply(lambda x: info(x/len(D))).agg('sum')
 
-    # Dataset (D) original entropy
-    totalEntropy = D.groupby(targetAttr).size().apply(lambda x: info(x/len(D))).agg('sum')
-    
-    # Calculate entropy after partitioning with attribute 'currAttr'
-    currAttr = 'Tempo'
-    # Partitionate dataset D using 'currAttr'
-    attrPartitionsCount = D.groupby([currAttr, targetAttr])[targetAttr].count()
-    # Create table with resultant partitions Dj
-    attrPartitions = D.groupby([currAttr]).agg({ targetAttr: 'count' })
+    # Partitionate dataset D using 'attr'
+    attrPartitionsCount = D.groupby([attr, targetAttr])[targetAttr].count()
+    # Create (auxiliary) table with resultant partitions Dj
+    attrPartitions = D.groupby([attr]).agg({ targetAttr: 'count' })
     # Calculate |Dj|/|D| for each partition Dj
-    attrPartitions['Prop'] = attrPartitionsCount.groupby(level=0).apply(lambda x: x.sum()/len(D))
+    attrPartitions['Prop'] = attrPartitionsCount.groupby(level=0).apply(lambda d: d.sum()/len(D))
     # Calculate entropy Info(Dj) for each parition Dj 
     attrPartitions['PartEntropy'] = attrPartitionsCount.groupby(level=0).agg(lambda x: np.sum(info(x/x.sum())))
     # Calculate |Dj|/|D| * Info(Dj) for each parition Dj
     attrPartitions['Entropy'] = attrPartitions['Prop'] * attrPartitions['PartEntropy']
     # Calculate entropy InfoA(D) of dataset after partitioning with attribute 'currAttr' 
-    currAttrEntropy = attrPartitions['Entropy'].agg('sum')
+    newEntropy = attrPartitions['Entropy'].agg('sum')
+
     # Calculate gain Gain(A)
-    currAttrGain = totalEntropy - currAttrEntropy
+    attrGain = currEntropy - newEntropy
     # Find attribute value that maximizes partition entropy 
     maxEntropyAttrValue = attrPartitions.idxmax()['PartEntropy']
 
-    print("> Resulting paritions for attribute: " + currAttr)
+    print("> Resulting paritions for attribute: " + attr)
     print(attrPartitions)
-    print("> Resulting entropy: " + str(currAttrEntropy))
-    print("> Resulting gain: " + str(currAttrGain))
+    print("> Resulting entropy: " + str(newEntropy))
+    print("> Resulting gain: " + str(attrGain))
     print("> Selected attribute value: " + maxEntropyAttrValue)
+    
+    return (attrGain, maxEntropyAttrValue)
+
+
+def main():
+    D = pd.read_csv('example.csv', sep=";")
+    targetAttr = 'Joga'
+    
+    # Calculate entropy after partitioning with attribute 'currAttr'
+    currAttr = 'Tempo'
+    
+    (gain, maxEntropyAttrValue) = infoGain(D, targetAttr, currAttr)
+    
+    print("###################################################")
+    selectedAttr = currAttr
+    selectedAttrValue = maxEntropyAttrValue
+    newD = D[D[selectedAttr] == selectedAttrValue]
 
 
 if __name__ == "__main__":
