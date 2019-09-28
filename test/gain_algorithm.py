525b2f792c35f881584eab9e7aa655ab06127c7a
Rubens Rech <rubensch@icloud.com>
2019-09-27 21:51:33 -0300

    Refactoring

--- test/gain_algorithm.py
+++ test/gain_algorithm.py
@@ -26,33 +26,21 @@ def infoGain(D, targetAttr, attr):
     attrPartitions['Entropy'] = attrPartitions['Prop'] * attrPartitions['PartEntropy']
     # Calculate entropy InfoA(D) of dataset after partitioning with attribute 'currAttr' 
     newEntropy = attrPartitions['Entropy'].agg('sum')
-
     # Calculate gain Gain(A)
     attrGain = currEntropy - newEntropy
-    # Find attribute value that maximizes partition entropy 
-    maxEntropyAttrValue = attrPartitions.idxmax()['PartEntropy']
-
-    if DEBUG:
-        print("> Resulting paritions for attribute: " + attr)
-        print(attrPartitions)
-        print("> Resulting gain: " + str(attrGain))
-        # print("> Resulting entropy: " + str(newEntropy))
-        # print("> Selected attribute value: " + maxEntropyAttrValue)
     
-    return (attrGain, maxEntropyAttrValue)
+    return attrGain
 
 def DecisionTree(D, targetAttr, attrs):
-    if DEBUG:
-        print("=============================================")
-        print("=============================================")
-
     # If all instances in D has same class
     Dclasses = D[targetAttr].unique()
     if (len(Dclasses) == 1):
+        # Return leaf node of only class
         return ClassNode(Dclasses[0], len(D))
 
     # If there are no more attributes
     if (len(attrs) == 0):
+        # Return leaf node of majority class
         majorityClass = D[targetAttr].value_counts().idxmax()
         return ClassNode(majorityClass, len(D))
 
@@ -60,77 +48,59 @@ def DecisionTree(D, targetAttr, attrs):
     maxGain = -1
     maxGainAttr = None
     for attr in attrs:
-        if DEBUG: print("===============================")
-        (gain, maxEntropyAttrValue) = infoGain(D, targetAttr, attr)
+        attrGain = infoGain(D, targetAttr, attr)
         if (gain > maxGain):
-            maxGain = gain
+            maxGain = attrGain
             maxGainAttr = attr
-    
-    if DEBUG:
-        print("\n===============================")
-        print("> RESULTS:", maxGainAttr, maxGain)
-        
-    # Select attribute with max InfoGain
-    selectedAttr = maxGainAttr
-    selectedAttrGain = maxGain
+            
     # Remove attribute from attributes list
-    attrs.remove(selectedAttr)
+    attrs.remove(maxGainAttr)
+    # Create node with selected attribute
+    node = AttrNode(maxGainAttr, maxGain)
 
-    node = AttrNode(selectedAttr, selectedAttrGain)
+    # > Node Split <
+    def nodeSplitAux(Dv, node, edgeValue):
+        # If partition is empty
+        if (len(Dv) == 0):
+            # Return leaf node labeled with most frequent class Yi in D
+            majorityClass = D[targetAttr].value_counts().idxmax()
+            node = ClassNode(majorityClass, len(D))
+        else:
+            # Connect node to sub-tree
+            node.setChild(edgeValue, DecisionTree(Dv, targetAttr, attrs))
+        return node
 
-    # Node Split: For each different value of the selected attribute
-    if DEBUG: print("> New partitions: ")
-
-    # Selected attribute is numeric
-    if (is_numeric_dtype(D[selectedAttr])):
-        cutoff = D[selectedAttr].mean()
-
+    # If selected attribute is numeric
+    if (is_numeric_dtype(D[maxGainAttr])):
+        # Define attribute division cutoff
+        cutoff = D[maxGainAttr].mean()
         # A <= cutoff
-        Dv = D[D[selectedAttr] <= cutoff]
-        if DEBUG: print("\n", Dv)
-        if (len(Dv) == 0):
-            # Return leaf node labeled with most frequent class Yi in D
-            majorityClass = D[targetAttr].value_counts().idxmax()
-            node = ClassNode(majorityClass, len(D))
-        else:
-            node.setChild('<= {0:.2f}'.format(cutoff), DecisionTree(Dv, targetAttr, attrs)) 
-
+        Dv = D[D[maxGainAttr] <= cutoff]
+        node = nodeSplitAux(Dv, node, '<= {0:.2f}'.format(cutoff))
         # A > cutoff
-        Dv = D[D[selectedAttr] > cutoff]
-        if DEBUG: print("\n", Dv)
-        if (len(Dv) == 0):
-            # Return leaf node labeled with most frequent class Yi in D
-            majorityClass = D[targetAttr].value_counts().idxmax()
-            node = ClassNode(majorityClass, len(D))
-        else:
-            node.setChild('> {0:.2f}'.format(cutoff), DecisionTree(Dv, targetAttr, attrs)) 
-
-    # Selected attribute is categorical
+        Dv = D[D[maxGainAttr] > cutoff]
+        node = nodeSplitAux(Dv, node, '> {0:.2f}'.format(cutoff))
+    # If selected attribute is categorical
     else:
-        attrValues = D[selectedAttr].unique()
+        attrValues = D[maxGainAttr].unique()
+        # For each different value of the selected attribute
         for v in attrValues:
             # Get resulting partition for each value of the selected attribute
-            Dv = D[D[selectedAttr] == v]
-            if DEBUG: print("\n", Dv)
-            # If partition is empty 
-            if (len(Dv) == 0):
-                # Return leaf node labeled with most frequent class Yi in D
-                majorityClass = D[targetAttr].value_counts().idxmax()
-                node = ClassNode(majorityClass, len(D))
-            else:
-                node.setChild(v, DecisionTree(Dv, targetAttr, attrs)) 
+            Dv = D[D[maxGainAttr] == v]
+            node = nodeSplitAux(Dv, node, v)
+
     return node
 
 def main():
     D = pd.read_csv('example.csv', sep=";")
     targetAttr = 'Joga'
 
-    # Initial state
     attrs = D.keys().tolist()
     attrs.remove(targetAttr)
 
     n = DecisionTree(D, targetAttr, attrs)
     print(n)
+    
     dot.view(cleanup=True)
 
 
