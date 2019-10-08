2e97df137001adf5c2e3e40cf16da855487e7c47
Rubens Rech <rubensch@icloud.com>
2019-10-08 15:54:23 -0300

    majority voting

--- lib/RandomForest.py
+++ lib/RandomForest.py
@@ -4,12 +4,26 @@ class RandomForest:
     def __init__(self, D, targetAttr, ntree):
         self.trees = []
         for i in range(ntree):
+            # Get bootstrap sets
             (trainSet, testSet) = self.bootstrap(D)
-            tree = DecisionTree.fromData(D, targetAttr)
-            tree.show()
+            # Create Decision Tree from training set
+            tree = DecisionTree.fromData(trainSet, targetAttr)
+            # Add tree to the ensemble
             self.trees.append(tree)
 
     def bootstrap(self, D, frac=0.8):
         train = D.sample(frac=frac, replace=True)
         test = D.drop(train.index)
         return (train, test)
+
+    def classify(self, instance):
+        preds = []
+        # Classify using each tree individually
+        for tree in self.trees:
+            preds.append(tree.classify(instance))
+        # Get the majority class
+        return max(set(preds), key=preds.count)
+
+    def render(self):
+        for tree in self.trees:
+            tree.show()
--- test/general.py
+++ test/general.py
@@ -17,9 +17,19 @@ def main():
     targetAttr = sys.argv[2]
     ntree = int(sys.argv[3])
 
+    # Read dataset
     D = pd.read_csv(datasetFile, sep=";")
-
+    # Build Random Forest
     forest = RandomForest(D, targetAttr, ntree)
+    # Test classification
+    instance = D.iloc[4]
+    print("> Test instance:")
+    print(instance)
+    print("> Prediction: %s" % forest.classify(instance))
+
+    if ntree <= 6:
+        forest.render()
+
 
 if __name__ == "__main__":
     main()
