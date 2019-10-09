6b84161fd031110ef8f23972f6772ee5683d70d1
Rubens Rech <rubensch@icloud.com>
2019-10-08 22:13:55 -0300

    working on cross validation

--- lib/RandomForest.py
+++ lib/RandomForest.py
@@ -1,27 +1,26 @@
 import time
 from DecisionTree import *
 
+def bootstrap(D, frac=0.8):
+    train = D.sample(frac=frac, replace=True)
+    test = D.drop(train.index)
+    return (train, test)
+
 class RandomForest:
     def __init__(self, D, targetAttr, ntree, graph=True):
         startTime = time.time()
-
         self.trees = []
+        # Training 'ntree' trees with different bootstraps
         for i in range(ntree):
             print("> Training trees... (%d/%d)" % (i+1, ntree), end='\r')
             # Get bootstrap sets
-            (trainSet, testSet) = self.bootstrap(D)
+            (trainSet, testSet) = bootstrap(D)
             # Create Decision Tree from training set
             tree = DecisionTree(trainSet, targetAttr, graph=graph)
             # Add tree to the ensemble
             self.trees.append(tree)
-            
         trainingTime = time.time() - startTime
         print("> Training duration: %f s" % trainingTime)
-
-    def bootstrap(self, D, frac=0.8):
-        train = D.sample(frac=frac, replace=True)
-        test = D.drop(train.index)
-        return (train, test)
 
     def classify(self, instance):
         preds = []
--- test/randomForest.py
+++ test/randomForest.py
@@ -8,17 +8,53 @@ sys.path.append(LIB_PATH)
 
 from RandomForest import *
 
+def getFolds(D, targetAttr, K):
+    DClasses = D.groupby([targetAttr]).agg({ targetAttr: 'count' })
+    DClasses['InstsPerFold'] = DClasses.apply(lambda x: x/K)
+
+    classSets = {}
+    classes = D[targetAttr].unique()
+    for c in classes:
+        classSets[c] = D[D[targetAttr] == c]
+
+    folds = {}
+    for i in range(K):
+        for j, c in enumerate(classes):
+            classInstsCount = int(DClasses.loc[c]['InstsPerFold'])
+            classSample = classSets[c].sample(n=classInstsCount)
+            if i not in folds:
+                folds[i] = classSample
+            else:
+                folds[i] = folds[i].append(classSample, sort=False)
+            classSets[c] = classSets[c].drop(classSample.index)
+
+    folds = list(folds.values())
+    print("%d folds" % len(folds))
+    print(folds)
+    for i in range(K):
+        print(">>> Fold %d <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" % i)
+        print(folds[i][targetAttr])
+        print(folds[i].groupby([targetAttr]).agg({ targetAttr: 'count' }).apply(lambda x: x/K))
+    
+    # print(classSets)
+    # print(DClasses)
+    # print(len(D)/K)
+
 def main():
     if (len(sys.argv) < 4):
-        print("Usage: python3 %s <dataset-csv> <target-attr> <ntree>" % sys.argv[0])
+        print("Usage: python3 %s <dataset-csv> <target-attr> <separator> <ntree>" % sys.argv[0])
         exit(-1)
 
     datasetFile = sys.argv[1]
     targetAttr = sys.argv[2]
-    ntree = int(sys.argv[3])
+    separator = sys.argv[3]
+    ntree = int(sys.argv[4])
 
     # Read dataset
-    D = pd.read_csv(datasetFile)
+    D = pd.read_csv(datasetFile, sep=separator)
+
+    getFolds(D, targetAttr, 10)
+    return
 
     # Build Random Forest
     forest = RandomForest(D, targetAttr, ntree, graph=False)
